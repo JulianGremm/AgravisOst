57cbd2d85057bfedcc05fb302532c0a2494debae
JulianGremm <jg@rolfjaeger-elektrotechnik.de>
2024-09-05 15:35:15 +0200

    4

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,19 +1,4 @@
 import streamlit as st
 
-import pandas as pd
-import os
-from datetime import datetime, timedelta
-
-from frequenz.client.common.metric import Metric
-from frequenz.client.reporting import ReportingApiClient
-
-# Ensure that environment variables are set
-try:
-    service_address = st.secrets["SCHNITTSTELLE"]
-    api_key = st.secrets["API_KEY"]
-except KeyError as e:
-    raise KeyError(f"Missing environment variable: {e}")
-
-client = ReportingApiClient(service_address=service_address, key=api_key)
 
 #st.write("This is just a sample page!")
--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -16,3 +16,44 @@ pg = st.navigation(nav)
 add_page_title(pg)
 
 pg.run()
+
+import pandas as pd
+import os
+from datetime import datetime, timedelta
+
+from frequenz.client.common.metric import Metric
+from frequenz.client.reporting import ReportingApiClient
+
+# Ensure that environment variables are set
+try:
+    service_address = st.secrets["SCHNITTSTELLE"]
+    api_key = st.secrets["API_KEY"]
+except KeyError as e:
+    raise KeyError(f"Missing environment variable: {e}")
+
+client = ReportingApiClient(service_address=service_address, key=api_key)
+
+
+
+data_listFürstenwalde = []
+
+
+for i in range(10):
+    match i:
+        case 0:
+            component_id = 394
+        case _:  # This is the syntax to handle all other cases
+            if i > 0:
+                component_id = 395 + i
+
+    data_sample = [
+        sample async for sample in client.list_single_component_data(
+            microgrid_id=116,
+            component_id=component_id,
+            metrics=[Metric.AC_ACTIVE_POWER],
+            start_dt=datetime.fromisoformat(f"2024-09-04T22:00:00"),
+            end_dt=datetime.fromisoformat(f"2024-09-05T00:00:00"),
+            resolution=300,#300
+        )
+    ]
+    data_listFürstenwalde.append(data_sample)
