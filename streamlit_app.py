563c133a1b03abf7115c82e82cf5fd042a0affa9
JulianGremm <jg@rolfjaeger-elektrotechnik.de>
2024-09-05 08:03:42 +0200

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+pg = st.navigation([st.Page("pages/page_1.py"), st.Page("pages/page_2.py")])
 
 st.sidebar.selectbox("Group", ["A","B","C"], key="group")
 st.sidebar.slider("Size", 1, 5, key="size")
