50a77ade27072d61db4f21deb49c61405686b668
JulianGremm <jg@rolfjaeger-elektrotechnik.de>
2024-09-05 07:54:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("Hello")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+
+st.sidebar.selectbox("Group", ["A","B","C"], key="group")
+st.sidebar.slider("Size", 1, 5, key="size")
+
+pg.run()
