eaa180d43e1cb1c01173f3db57659c5ef30d4e13
JulianGremm <jg@rolfjaeger-elektrotechnik.de>
2024-09-05 08:00:46 +0200

    Test

--- page_2.py
+++ page_2.py
@@ -1,1 +1,5 @@
 
+st.title("Hello")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.title("Hello")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
 pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
 
 st.sidebar.selectbox("Group", ["A","B","C"], key="group")
