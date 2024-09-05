cc4f008419b7722c016778250767c1797d16c6ea
JulianGremm <jg@rolfjaeger-elektrotechnik.de>
2024-09-05 10:24:48 +0200

    9

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,5 +1,17 @@
+from pathlib import Path
 
-st.title("Ciao")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
+
+st.write("`streamlit_app.py` contents:")
+code = Path("example_app/streamlit_app.py").read_text()
+
+st.code(code, language="python")
+
+location = "pages_sections.toml" if st.session_state["use_sections"] else "pages.toml"
+st.write(f"`{location}` contents:")
+toml_code = Path(f".streamlit/{location}").read_text()
+st.code(toml_code, language="toml")
+
+st.help(get_nav_from_toml)
+st.help(add_page_title)
--- pages/page_2.py
+++ pages/page_2.py
@@ -1,5 +1,3 @@
+import streamlit as st
 
-st.title("Hello")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("This is just a sample page!")
